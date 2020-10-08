433f3ec74f2c24b105437da581abd372e034df49
Renan <rvidal@bdsdatasolution.com.br>
2020-10-08 18:37:01 -0300

    prmeiro commit

---
+++ XF.LocalDB/XF.LocalDB/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.LocalDB.Model;
+using XF.LocalDB.AlunoViewModels;
+using XF.LocalDB;
+using XF.LocalDB.View;
+
+namespace XF.LocalDB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new NavigationPage(new View.Aluno.MainPage());
+        }
+        static Aluno alunoModel;
+        public static Aluno AlunoModel
+        {
+            get
+            {
+                if (alunoModel == null)
+                {
+                    alunoModel = new Aluno();
+                }
+                return alunoModel;
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XF.LocalDB/XF.LocalDB/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using XF.LocalDB.View.Aluno;
+using XF.LocalDB.AlunoViewModels;
+
+namespace XF.LocalDB
+{
+    public partial class MainPage : ContentPage
+    {
+
+        AlunoViewModel vmAluno;
+        public MainPage()
+        {
+            vmAluno = new AlunoViewModel();
+            BindingContext = vmAluno;
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            vmAluno = new AlunoViewModel();
+            BindingContext = vmAluno;
+            base.OnAppearing();
+        }
+        private void OnNovo(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new NovoAlunoView());
+        }
+        private void OnAlunoTapped(object sender,
+        ItemTappedEventArgs args)
+        {
+            var selecionado = args.Item as XF.LocalDB.Model.Aluno;
+            DisplayAlert("Aluno selecionado", "Aluno: " + selecionado.Id, "OK");
+        }
+    }
+}
+
